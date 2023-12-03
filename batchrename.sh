50784f846b35604af2f1701af50fee15c71a45cf
braz-bogdan156 <105545772+braz-bogdan156@users.noreply.github.com>
2023-12-03 18:41:47 +0200

    Update batchrename.sh

--- batchrename.sh
+++ batchrename.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
-for i in {1..25}; do
-    name="bohdanvirt"
-    number=$((i + $(ls -1q "${name}"* 2>/dev/null | wc -l)))
-    touch "${name}${number}"
+if [ "$#" -ne 3 ]; then
+    echo "Usage: $0 <directory> <original_extension> <new_extension>"
+    exit 1
+fi
+
+directory=$1
+original_extension=$2
+new_extension=$3
+
+for file in "$directory"/*."$original_extension"; do
+    new_file="${file%.$original_extension}.$new_extension"
+    echo "Переіменовую $file на $new_file"
 done
